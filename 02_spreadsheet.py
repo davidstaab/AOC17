93f524823415da634aea1fd58cdfb31550c4be5b
David Staab <david.staab@gmail.com>
2018-01-17 22:05:16 -0600

    tweaked solution to ex 2, pt 2 to fix a speed optimization bug (wasn't breaking out of both nested loops)

--- 02_spreadsheet.py
+++ 02_spreadsheet.py
@@ -1,6 +1,21 @@
 checksum = 0
 divis_sum = 0
 
+
+def divide(divisor: float, dividends: list) -> int:
+    """
+    Divides a value by each other value in a list. When a clean division is found, returns the quotient. If no clean
+    division is found, returns 0.
+
+    :return: Quotient, if a clean division is found, or 0
+    """
+    for dividend in dividends:
+        rem = divisor % dividend
+        if rem == 0:
+            return int(divisor / dividend)
+    return 0
+
+
 with open('./spreadsheet.txt') as sheet:
     for line in sheet:
         cells = [int(i) for i in line.strip().split(sep='\t')]
@@ -11,11 +26,10 @@ with open('./spreadsheet.txt') as sheet:
         # PART 2
         cells.sort(reverse=True)  # Sort desc to speed up divisibility search, since a / b < 1 is invalid
         for i in range(len(cells)):
-            for other in cells[i+1:]:
-                rem = cells[i] % other
-                if rem == 0:
-                    divis_sum += int(cells[i] / other)
-                    break  # End search within line once found
+            val = divide(cells[i], cells[i + 1:])
+            divis_sum += val
+            if val != 0:
+                break
 
 print('Checksum: {}'.format(checksum))
 print('Evenly Divisible Pairs Sum: {}'.format(divis_sum))
