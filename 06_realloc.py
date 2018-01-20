6f3e41e7ac531a43728c06aba2c20086ae1fe24a
David Staab <david.staab@gmail.com>
2018-01-20 17:23:28 -0600

    ex6, pt2

--- 06_realloc.py
+++ 06_realloc.py
@@ -18,20 +18,33 @@ if __name__ == '__main__':
 
     with open('./memory_banks.txt') as file:
         banks = [int(x) for x in file.read().split()]
-
         configs = [hash(tuple(banks))]
 
+        cycle_len = 0
+        recur_len = 0
+        did_once = False
         while 1:
             fullest_idx = banks.index(max(banks))
             blocks = banks[fullest_idx]
             banks[fullest_idx] = 0
+
             idx = fullest_idx
             while blocks > 0:
                 idx = incr(idx, len(banks))
                 blocks -= 1
                 banks[idx] += 1
+
             config = hash(tuple(banks))
             if config in configs:
-                break
+                if not did_once:
+                    cycle_len = len(configs)
+                    configs = []
+                    did_once = True
+                else:
+                    recur_len = len(configs)
+                    break
             configs.append(config)
-        print('Steps to infinite loop: {}'.format(len(configs)))
+
+        print('Part 1: Steps to infinite loop: {}'.format(cycle_len))
+        print('Part 2: Steps to second recurrence: {}'.format(recur_len))
+
