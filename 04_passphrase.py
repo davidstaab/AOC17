b282949a930ede25b7f7c36489090b5ac4a2c83e
David Staab <david.staab@gmail.com>
2018-01-20 15:56:31 -0600

    ex4, pt 2

--- 04_passphrase.py
+++ 04_passphrase.py
@@ -5,13 +5,36 @@
 
 if __name__ == '__main__':
     with open('./passphrases.txt') as file:
-        valid_ct = 0
+        unique_ct = 0
+        no_perm_ct = 0
+
         for line in file:
             words = line.rstrip().split(sep=' ')  # Remove '\n' from `line` so it doesn't show up in words[-1]
 
-            valid_ct += 1  # presume unique words
+            # PART 1
+            unique_ct += 1  # presume unique words
             for w in words:
                 if words.count(w) > 1:
-                    valid_ct -= 1  # remove presumption
+                    unique_ct -= 1  # remove presumption
                     break
-        print('Valid passphrases: {}'.format(valid_ct))
+
+            # PART 2
+            no_perm_ct += 1  # presume no permutations
+            for i, base in enumerate(words):
+                pre_ct = no_perm_ct
+                for test in words[i + 1:]:
+                    test = list(test)
+                    for c in base:
+                        try:
+                            test.remove(c)
+                        except ValueError:  # "c not in list"
+                            test = True  # 'True' fails final check below
+                            break
+                    if not test:  # if all chars were mapped from `base` to `test`...
+                        no_perm_ct -= 1  # remove presumption
+                        break
+                if no_perm_ct < pre_ct:
+                    break  # Don't test remaining words in line. Failure already found.
+
+        print('Unique-word passphrases: {}'.format(unique_ct))
+        print('Non-permuted passphrases: {}'.format(no_perm_ct))
