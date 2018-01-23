e30dea82964d7408b033b51b382025e2390c4c89
David Staab <david.staab@gmail.com>
2018-01-23 09:34:07 -0600

    ex8, pt1 first attempt

---
+++ 08_registers.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+
+    exc_globals = {'__builtins__': None}
+    exc_locals = {}
+
+    with open('./registers.txt') as file:
+        for line in file:
+            fields = line.rstrip().split(sep=' ').__iter__()
+            var = next(fields)
+            opr = next(fields).replace('inc', '+=').replace('dec', '-=')
+            val = next(fields)
+            cond = [f for f in fields]
+            cond_var = cond[1]
+
+            for v in [var, cond_var]:
+                exec('try: {}\nexcept: {} = 0'.format(v, v), exc_globals, exc_locals)  # initialize var
+
+            cmd = '{} {} {} {}'.format(var, opr, val, ' '.join(cond)) + ' else 0'
+            print(cmd)
+            exec(cmd, exc_globals, exc_locals)
+
+        print(exc_locals)
+        print(exc_locals[max(exc_locals)])
