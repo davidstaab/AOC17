c02f54868e464d8ad3ecddcc1e6c6b2e05cc55fe
David Staab <david.staab@gmail.com>
2018-01-23 22:08:08 -0600

    ex9, pt2

--- 09_stream.py
+++ 09_stream.py
@@ -45,17 +45,28 @@ class GarbageChecker:
     def __init__(self, skip=True):
         self.skip = skip
         self.group = False
+        self._count = 0
 
     def is_garbage(self, char: str) -> bool:
         if self.group:
+            # inside a garbage group
             if char == '>':
+                # ending the group
                 self.group = False
+            else:
+                self._count += 1
             return True
         elif char == '<':
+            # starting a garbage group
             self.group = True
             return True
         else:
+            # outside a garbage group
             return False
+
+    @property
+    def count(self):
+        return self._count
 
 
 def gen_token(stream, skip_bang: bool=True, skip_garbage: bool=True) -> str:
@@ -66,18 +77,29 @@ def gen_token(stream, skip_bang: bool=True, skip_garbage: bool=True) -> str:
     :param skip_garbage: When true, skip and thrwo away everything in a garbage group
     :return: Token from `stream`
     """
+
+    # [dstaab] Note: `garbage_ct` is what happens when you try to quickly adapt (hack) an OO design change to accomodate
+    # a new requirement specification. I hacked the property into the class that encapsulates the needed info, then had
+    # to poll it excessively in this loop -- AFTER calling the method that updates it, which breaks the abstraction of
+    # the class! -- and dump it out to the main app that actually needs it, because this generator function itself
+    # encapsulates the class that the app needs to ask for its property value. What a mess.
+
     gc = GarbageChecker(skip_garbage)
     for char, ct in BangSkipper(stream, skip=skip_bang):
-        if not gc.is_garbage(char):
-            yield char, ct
+        g = gc.is_garbage(char)
+        garbage_ct = gc.count
+        if not g:
+            yield char, ct, garbage_ct
 
 
 if __name__ == '__main__':
 
     sum_levels = 0
+    sum_garbage = 0
     with open('./stream.txt') as file:
         cur_level = 0
-        for tok, ct in gen_token(file):
+        for tok, ct, g_ct in gen_token(file):
+            sum_garbage = g_ct
             print('{}:\t{}'.format(ct, tok))
             if tok == '{':
                 cur_level += 1
@@ -85,3 +107,4 @@ if __name__ == '__main__':
             elif tok == '}' and cur_level > 0:
                 cur_level -= 1
     print('Sum of all nesting levels: {}'.format(sum_levels))
+    print('Sum of all garbage characters: {}'.format(sum_garbage))
