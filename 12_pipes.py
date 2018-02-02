73b8787436b982268872bc09922dd54f8cd4e790
David Staab <david.staab@gmail.com>
2018-02-01 22:06:01 -0600

    ex12, pt 2

--- 12_pipes.py
+++ 12_pipes.py
@@ -1,21 +1,40 @@
+def new_group(graph: list) -> set:
+    for i in range(len(graph)):
+        if graph[i] is not None:
+            return set(graph[i])
+    return None
+
+
+def fill_group(graph: list, to_visit: set) -> set:
+    visited = set()
+    while 1:
+        try:  # to find a node in `to_visit` that hasn't already been visited
+            curr = to_visit.pop()
+            while curr in visited:
+                curr = to_visit.pop()
+        except KeyError:
+            break
+        visited.add(curr)
+        to_visit.update({_ for _ in graph[curr]})
+        graph[curr] = None
+    return visited
+
+
 if __name__ == '__main__':
-    anchor_id = 0
+
+    graph = []
     with open('./pipes.txt') as file:
         graph = file.readlines()
-        for i, line in enumerate(graph):
-            graph[i] = [int(x) for x in line.split(' <-> ')[1].split(',')]
+    for i, line in enumerate(graph):
+        graph[i] = [int(x) for x in line.split(' <-> ')[1].split(',')]
 
-        to_visit = {anchor_id}
-        visited = set()
-        while 1:
-            try:
-                curr = to_visit.pop()
-                while curr in visited:
-                    curr = to_visit.pop()
-            except KeyError:
-                break
+    groups = []
+    while any(graph):
+        to_visit = new_group(graph)
+        if to_visit is None:
+            break
+        groups.append(fill_group(graph, to_visit))
 
-            visited.add(curr)
-            to_visit.update({_ for _ in graph[curr]})
-
-        print('Programs in group {}: {}'.format(anchor_id, len(visited)))
+    print('{} groups found.'.format(len(groups)))
+    for g in groups:
+        print('Programs in group {}: {}'.format(min(g), len(g)))
