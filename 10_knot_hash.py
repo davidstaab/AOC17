a4d1ceda18609ba84622856fd45e8537b57d706c
David Staab <david.staab@gmail.com>
2018-02-07 11:25:55 -0600

    more improvements to prep for ex 14. i realize now that i didn't get the concept of the knot hash from ex 10's description when i solved that problem.

--- 10_knot_hash.py
+++ 10_knot_hash.py
@@ -4,29 +4,24 @@ from proj import kh
 if __name__ == '__main__':
 
     # PART 1
-    p = kh.KnotHash(256)
+    p = kh.KnotHash()
     with open('./knot_lengths.txt') as file:
         lengths = [int(x) for x in file.read().strip().split(sep=',')]
 
         for length in lengths:
-            p.knot(length)
+            p.knot1(length)
 
-    val1 = p.values[0]
-    val2 = p.values[1]
+    val1 = p.sparse_hash()[0]
+    val2 = p.sparse_hash()[1]
     print('Part 1:')
     print('First two numbers in list: {}, {}'.format(val1, val2))
     print('Their product: {}'.format(val1 * val2))
 
     # PART 2
-    p = kh.KnotHash(256)
-    seq_end = [17, 31, 73, 47, 23]
-    rounds = 64
+    p = kh.KnotHash()
     with open('./knot_lengths.txt') as file:
-        lengths = [ord(c) for c in file.read().strip()]  # interpret chars in file as ASCII
-        lengths += seq_end
-        for _ in range(rounds):
-            for length in lengths:
-                p.knot(length)
+        lengths = file.read().strip()
+        p.knot_all(lengths)
 
     print('\nPart 2:')
     print('Dense hash: {}'.format(kh.dh_hex(p.dense_hash(block_size=16))))
--- proj/kh.py
+++ proj/kh.py
@@ -1,4 +1,4 @@
-from collections.abc import MutableSequence, Iterable, Sequence
+from collections.abc import MutableSequence, Iterable
 
 
 class CircularBuffer(MutableSequence):
@@ -28,19 +28,24 @@ class CircularBuffer(MutableSequence):
         collection.
         """
         _s = len(self._coll)
-        if key.step is not None:
-            if not 0 <= key.step <= 1:
-                raise NotImplementedError('Step size > 1')
-        if key.start is not None and key.stop is not None:
+        if not (key.step == 1 or key.step is None):
+            raise NotImplementedError('Step size > 1')
+
+        if key.start is None or key.stop is None:
+            start = 0 if key.start is None else key.start
+            stop = _s if key.stop is None else key.stop
+        else:
             if key.stop < key.start:
-                raise IndexError('Stop must be greater or equal to Start.')
+                raise IndexError(
+                    'Stop must be greater or equal to Start. Add len(self) to Stop if slice should wrap buffer.'
+                )
             if key.stop - key.start > _s:
                 raise IndexError('Slice span is longer than the collection.')
+            span = key.stop - key.start
+            start = key.start % _s
+            stop = start + span
 
         # Map slice bounds into range of [0 : 2*len - 1]
-        span = key.stop - key.start
-        start = key.start % _s
-        stop = start + span
         return slice(start, stop, key.step)
 
     def __getitem__(self, key):
@@ -96,25 +101,39 @@ class KnotHash:
     of a skip value
     """
 
-    def __init__(self, length: int):
-        self._string = CircularBuffer(range(length))
-        self._ptr = 0
-        self._skip = 0
+    salt = [17, 31, 73, 47, 23]
+    rounds = 64
 
-    def knot(self, length: int):
-        sub = self._string[self._ptr : self._ptr + length]
+    def knot1(self, length: int):
+        sub = self._buf[self._ptr: self._ptr + length]
         sub.reverse()
-        self._string[self._ptr: self._ptr + length] = sub
+        self._buf[self._ptr: self._ptr + length] = sub
 
         self._ptr += length + self._skip
         self._skip += 1
 
+    def knot_all(self, lengths: Iterable):
+        if isinstance(lengths, str):
+            lengths = [ord(c) for c in lengths]  # interpret at ASCII
+        lengths += self.salt
+        for _ in range(self.rounds):
+            for length in lengths:
+                self.knot1(length)
+
+    def __init__(self, lengths: Iterable=None):
+        self._buf = CircularBuffer(range(256))
+        self._ptr = 0
+        self._skip = 0
+
+        if lengths is not None:
+            self.knot_all(lengths)
+
     def dense_hash(self, block_size: int) -> list:
-        hash_size = int(len(self._string) / block_size)
+        hash_size = int(len(self._buf) / block_size)
         dense_hash = []
         for i in range(hash_size):
             block_start = i * block_size
-            dense_hash.append(self._string[block_start : block_start + block_size])
+            dense_hash.append(self._buf[block_start: block_start + block_size])
         for i, block in enumerate(dense_hash):
             hash_val = block[0]
             for j in block[1:]:
@@ -122,9 +141,8 @@ class KnotHash:
             dense_hash[i] = hash_val
         return dense_hash
 
-    @property
-    def values(self) -> CircularBuffer:
-        return self._string
+    def sparse_hash(self) -> list:
+        return self._buf[:]
 
 
 def dh_hex(dense_hash: list) -> str:
