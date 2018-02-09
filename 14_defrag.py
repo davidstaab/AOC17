87843772fe11927e14c125ea6a98e65e339f1d55
David Staab <david.staab@gmail.com>
2018-02-08 23:49:24 -0600

    ex14, pt 2. scipy did it for me, lol.

--- 14_defrag.py
+++ 14_defrag.py
@@ -1,10 +1,21 @@
 from proj import kh
+import numpy as np
+from scipy.ndimage import label
 
 seed = 'jzgqcdpd'
 
 if __name__ == '__main__':
+
+    hashes = []
     ones = 0
     for i in range(128):
         dh = kh.KnotHash(f'{seed}-{i}').dense_hash()
-        ones += kh.dh_bin(dh).count('1')
+        dhb = kh.dh_bin(dh)
+        ones += dhb.count('1')  # PART 1
+        hashes.append([int(_) for _ in dhb])  # PART 2
     print(f'Used squares: {ones}')
+
+    # PART 2 CONT'd
+    disk = np.array(hashes)
+    labeled, regions_ct = label(disk)  # default structure is cross shape needed here
+    print(f'Used regions: {regions_ct}')
