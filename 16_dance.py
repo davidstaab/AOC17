f16365f10d48a5e094990b0ba711d9c23d4e3c2b
David Staab <david.staab@gmail.com>
2018-02-09 14:30:35 -0600

    tried ex 16, pt 2 again with speed optimizations and a cycle detector. found a nice cycle, but not using it correctly yet.

--- 16_dance.py
+++ 16_dance.py
@@ -1,6 +1,3 @@
-import re
-
-
 def spin(count: int):
     global line
     line = line[-count:] + line[:-count]
@@ -8,35 +5,35 @@ def spin(count: int):
 
 def exchange(idx1: int, idx2: int):
     global line
-    t = line[idx1]
-    line[idx1] = line[idx2]
-    line[idx2] = t
+    line[idx1], line[idx2] = line[idx2], line[idx1]
 
 
 def partner(val1: str, val2: str):
     global line
     idx1 = line.index(val1)
     idx2 = line.index(val2)
-    exchange(idx1, idx2)
+    line[idx1], line[idx2] = line[idx2], line[idx1]
 
 
-def dance(x: str):
-    instr = x[0]
-    if instr == 's':
-        spin(int(x[1:]))
-    elif instr == 'x':
-        a, b = re.findall('[0-9]+', x[1:])  # use re to find multi-digit arguments
-        exchange(int(a), int(b))
-    elif instr == 'p':
-        partner(x[1], x[3])
-    else:
-        raise ValueError(f'Invalid instruction code: {instr}')
+def dance(instrs: list):
+    for x in instrs:
+        instr = x[0]
+        if instr == 's':
+            spin(int(x[1:]))
+        elif instr == 'x':
+            a, b = map(int, x[1:].split('/'))
+            exchange(int(a), int(b))
+        elif instr == 'p':
+            partner(x[1], x[3])
+        else:
+            raise ValueError(f'Invalid instruction code: {instr}')
 
 
 if __name__ == '__main__':
     length = 16
     start = ord('a')
     line = [chr(x) for x in range(start, start + length)]
+    line_init = line[:]
     print('Initial order: ' + ''.join(line))
 
     instr_set = []
@@ -44,28 +41,37 @@ if __name__ == '__main__':
         instr_set = file.read().strip().split(',')
 
     # PART 1
-    for x in instr_set:
-        dance(x)
-    print('Pt 1 final order: ' + ''.join(line))
+    dance(instr_set)
+    print('Pt 1 final order: ' + ''.join(line), end='\n\n')
 
     # PART 2
-    # Confession: This problem falls into the domain of group theory in mathematics, which is way beyond my
-    # understanding of algebra. I'm going to implement the solution detailed here to practice coding:
-    # https://www.reddit.com/r/adventofcode/comments/7k5mrq/spoilers_in_title2017_day_16_part_2_cycles/drcd8vj/
-    memo = {}  # k: starting permutation, v: final permutation
-    memo_acc_ct = 0
-    for i in range(int(1e9 - 1)):
-        if i % int(1e6) == 0:
-            print(f'Iteration {i} - {memo_acc_ct} memo accesses - memo size {len(memo)}')
-        key = ''.join(line)
-        if key in memo:
-            # skip instructions, just grab result from memo
-            memo_acc_ct += 1
-            line = memo[key]
+    # Confession: I had to read other people's answers to understand how this problem is solved. I'm doing it with two
+    # optimizations: A memoized dance solver using `dance_memo` and a cycle finder using `cycle`. [NOTE] I'm assuming
+    # a cycle is an integer multiple of dances. If the cycle is a multiple of instructions but not a multiple of dances,
+    # the code becomes a bit more complex, but it's still feasible.
+
+    dance_memo = {}  # k: starting permutation, v: final permutation
+    cycle = [''.join(line_init), ''.join(line)]  # permutations already seen in Pt 1
+    key = ''.join(line)
+    for i in range(int(1e9) - 1):
+        if key in dance_memo:
+            line = dance_memo[key]
         else:
-            for x in instr_set:
-                dance(x)
-            # add result to memo
-            memo[key] = line
+            dance(instr_set)
+            dance_memo[key] = line
+
+        key = ''.join(line)
+        if key in cycle:  # Cycle found. This # of dances returns the string back to its original state.
+            break
+        cycle.append(key)
+    print(f'Cycle found with length {len(cycle)}')
+
+    line = line_init
+    for i in range(int(1e9) % len(cycle)):
+        if key in dance_memo:
+            line = dance_memo[key]
+        else:
+            dance(instr_set)
+            dance_memo[key] = line
+
     print('Pt 2 final order: ' + ''.join(line))
-    print(f'{memo_acc_ct} total memo accesses - Final memo size {len(memo)}')
