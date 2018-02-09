2711e51a0f3dd9a0a6ff332e979d0f849a0a7c15
David Staab <david.staab@gmail.com>
2018-02-09 00:55:20 -0600

    ex 15, pt 2

--- 15_generators.py
+++ 15_generators.py
@@ -3,17 +3,10 @@ mult_b = 48271
 div = 2147483647
 start_a = 289
 start_b = 629
-count = 40000000
-
-# TEST INPUTS
-# start_a = 65
-# start_b = 8921
-# count = 5
 
 
-def gen() -> tuple:
-    a = start_a
-    b = start_b
+def gen_pt1(count: int) -> tuple:
+    a, b = start_a, start_b
     yield a, b
     for _ in range(count - 1):
         a = (a * mult_a) % div
@@ -21,12 +14,52 @@ def gen() -> tuple:
         yield a, b
 
 
+def gen_pt2(count: int) -> tuple:
+
+    check_a = 4
+    check_b = 8
+
+    def get(val: int, multiplier: int, multiple: int) -> int:
+        skip = True
+        while skip:
+            val = (val * multiplier) % div
+            skip = val % multiple
+        return val
+
+    a, b = start_a, start_b
+    if a % check_a == 0 and b % check_b == 0:
+        yield a, b
+        count -= 1
+    for _ in range(count):
+        a = get(a, mult_a, check_a)
+        b = get(b, mult_b, check_b)
+        yield a, b
+
+
 if __name__ == '__main__':
 
     # PART 1
-    match_ct = 0
-    for a, b in gen():
-        # print(f'{bin(a)}, {bin(b)}')
-        match_ct += bin(a)[-16:] == bin(b)[-16:]
-    print(f'Matches: {match_ct}')
+    # count = 40000000
+    # match_ct = 0
 
+    # TEST INPUTS
+    # start_a = 65
+    # start_b = 8921
+    # count = 5
+
+    # for a, b in gen_pt1(count):
+    #     match_ct += bin(a)[-16:] == bin(b)[-16:]
+
+    # PART 2
+    count = 5000000
+    match_ct = 0
+
+    # TEST INPUTS
+    # start_a = 65
+    # start_b = 8921
+    # count = 1056
+
+    for a, b in gen_pt2(count):
+        match_ct += bin(a)[-16:] == bin(b)[-16:]
+
+    print(f'Matches: {match_ct}')
