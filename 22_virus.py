89bc1bbca87d24ee585cc3b0d48f278d8843e053
David Staab <david.staab@gmail.com>
2018-02-22 02:34:55 -0600

    ex 22, pt 2

--- 22_virus.py
+++ 22_virus.py
@@ -1,3 +1,8 @@
+# Note: I wrote this using sets and tuples because it looks like memory was going to be more valuable (on a
+# "near-infinite grid") than CPU. Also, pt. 1 only needed a boolean flag for each location. Whoops. It would probably
+#  be optimized for part 2 by defining the grid and locations using NumPy arrays for in-place access and trivial
+#  addition functions. The array could hold 4-bit values, or just strings if that's fast enough.
+
 # define all these names so debugging is easier
 left = (0, -1)  # dec col
 up = (-1, 0)    # dec row
@@ -50,6 +55,10 @@ if __name__ == '__main__':
     # cur = (1, 1)
     # step = up
 
+    # PART 1
+    infected_init = infected.copy()
+    cur_init = cur
+    step_init = step
     new_infections = 0
     for _ in range(10000):
         if cur in infected:
@@ -60,5 +69,30 @@ if __name__ == '__main__':
             infected.add(cur)
             new_infections += 1
         cur = tuple_add(cur, step)
-
     print(f'Part 1: {new_infections} new infections')
+
+    # PART 2
+    infected = infected_init
+    weakened, flagged = set(), set()
+    cur = cur_init
+    step = step_init
+    new_infections = 0
+    for _ in range(int(10e6)):
+        if cur in infected:
+            step = turn[step]['R']
+            infected.remove(cur)
+            flagged.add(cur)
+        elif cur in weakened:
+            # do not turn
+            weakened.remove(cur)
+            infected.add(cur)
+            new_infections += 1
+        elif cur in flagged:
+            step = turn[step]['R']
+            step = turn[step]['R']
+            flagged.remove(cur)
+        else:
+            step = turn[step]['L']
+            weakened.add(cur)
+        cur = tuple_add(cur, step)
+    print(f'Part 2: {new_infections} new infections')
